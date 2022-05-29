a97a571700463f43bc3b4b42c404467dda8282b4
adner <67954372+32fca06c@users.noreply.github.com>
2022-05-29 23:59:09 +0500

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -4,12 +4,8 @@ echo fastestmirror=True>>/etc/dnf/dnf.conf
 echo max_parallel_downloads=20>>/etc/dnf/dnf.conf
 dnf install fedora-workstation-repositories https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 
-# virtualization
-dnf install @virtualization
-systemctl start libvirtd
-systemctl enable libvirtd
-
 # xanmod kernel
+dnf copr enable rmnscnce/kernel-xanmod -y
 dnf install kernel-xanmod-edge kernel-xanmod-edge-devel kernel-xanmod-edge-devel-matched kernel-xanmod-edge-headers
 reboot
 
@@ -22,3 +18,11 @@ systemctl enable nvidia-{suspend,resume,hibernate}
 nano /etc/default/grub
 grub2-mkconfig -o /boot/grub2/grub.cfg
 reboot
+
+# virtualization
+dnf install @virtualization
+systemctl start libvirtd
+systemctl enable libvirtd
+
+# apps
+dnf install telegram discord google-chrome-stable remmina
