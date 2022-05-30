b2a65bb4e849d4a39e255781efbf8f8780a0434e
adner <67954372+32fca06c@users.noreply.github.com>
2022-05-30 18:47:55 +0500

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -5,8 +5,8 @@ echo max_parallel_downloads=20>>/etc/dnf/dnf.conf
 dnf install fedora-workstation-repositories https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 dnf install dnf-automatic -y
 env EDITOR='gedit -w' sudoedit /etc/dnf/automatic.conf
-systemctl start --now dnf-automatic.timer
-systemctl enable --now dnf-automatic.timer
+systemctl start dnf-automatic.timer
+systemctl enable dnf-automatic.timer
 
 # xanmod kernel
 dnf copr enable rmnscnce/kernel-xanmod -y
