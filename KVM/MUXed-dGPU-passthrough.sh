434b70f12b97e810f816bfc6db0bc576b30019bc
adner <67954372+32fca06c@users.noreply.github.com>
2022-06-04 15:05:02 +0500

    Update MUXed-dGPU-passthrough.sh

--- KVM/MUXed-dGPU-passthrough.sh
+++ KVM/MUXed-dGPU-passthrough.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
 nano /etc/default/grub
-intel_iommu=on iommu=pt
-grub2-mkconfig -o /boot/grub2/grub.cfg
-reboot
-
-CheckIOMMUGroups.sh
-nano /etc/default/grub
-rd.driver.pre=vfio_pci vfio-pci.ids=10de:1c20,10de:10f1
+intel_iommu=on iommu=pt rd.driver.pre=vfio_pci vfio-pci.ids=10de:1c20,10de:10f1
 grub2-mkconfig -o /boot/grub2/grub.cfg
 reboot
 
 lspci -nnk -s 1:
 
 dnf install git python2 iasl nasm subversion perl-libwww-perl vim dos2unix gcc gcc-c++ patch libuuid-devel -y
-
+cd /var/lib/libvirt/
 git clone --recurse-submodules https://github.com/tianocore/edk2.git
