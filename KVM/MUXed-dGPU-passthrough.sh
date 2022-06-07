664329c0894e46fad1d66c1c04da9e7e78cbc601
adner <67954372+32fca06c@users.noreply.github.com>
2022-06-07 13:02:51 +0500

    Update MUXed-dGPU-passthrough.sh

--- KVM/MUXed-dGPU-passthrough.sh
+++ KVM/MUXed-dGPU-passthrough.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-sudo usermod -a -G libvirt $(whoami)
-nano /etc/default/grub
+#nano /etc/default/grub
 intel_iommu=on iommu=pt rd.driver.pre=vfio_pci vfio-pci.ids=10de:1c20,10de:10f1
 grub2-mkconfig -o /boot/grub2/grub.cfg
 reboot
 
-lspci -nnk -s 1:
+#lspci -nnk -s 1:
 
-dnf install git python2 iasl nasm subversion perl-libwww-perl vim dos2unix gcc gcc-c++ patch libuuid-devel -y
+#dnf install git python2 iasl nasm subversion perl-libwww-perl vim dos2unix gcc -y
+dnf install dos2unix gcc-c++ patch libuuid-devel -y
 cd /var/lib/libvirt/
 cp /home/adner/kvm/SSDT1.dat SSDT1.dat
 git clone --recurse-submodules https://github.com/tianocore/edk2.git
 cd edk2/OvmfPkg/AcpiPlatformDxe
 cp /home/adner/kvm/vbios.rom vbios.rom
 xxd -i vbios.rom vrom.h
-nano vrom.h
+#nano vrom.h
 wget https://github.com/jscinoz/optimus-vfio-docs/files/1842788/ssdt.txt -O ssdt.asl
-nano ssdt.asl
+#nano ssdt.asl
 iasl -f ssdt.asl
 xxd -c1 Ssdt.aml | tail -n +37 | cut -f2 -d' ' | paste -sd' ' | sed 's/ //g' | xxd -r -p > vrom_table.aml
 xxd -i vrom_table.aml | sed 's/vrom_table_aml/vrom_table/g' > vrom_table.h
@@ -28,6 +28,6 @@ patch -p1 < nvidia-hack-2.diff
 unix2dos OvmfPkg/AcpiPlatformDxe/QemuFwCfgAcpi.c
 make -C BaseTools
 . ./edksetup.sh BaseTools
-nano Conf/target.txt
+#nano Conf/target.txt
 build
 yes | cp Build/OvmfX64/DEBUG_GCC5/FV/OVMF_VARS.fd /var/lib/libvirt/qemu/nvram/Win10_VARS.fd
