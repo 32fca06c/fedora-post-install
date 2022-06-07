404b544fc4eb64dcb580cc954b0df2259129dc7f
adner <67954372+32fca06c@users.noreply.github.com>
2022-06-07 13:11:34 +0500

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -9,6 +9,8 @@ echo name=onlyoffice repo>>/etc/yum.repos.d/onlyoffice.repo
 echo baseurl=https://download.onlyoffice.com/repo/centos/main/noarch/>>/etc/yum.repos.d/onlyoffice.repo
 echo gpgcheck=0>>/etc/yum.repos.d/onlyoffice.repo
 echo enabled=1>>/etc/yum.repos.d/onlyoffice.repo
+#
+dnf config-manager --set-enabled 
 # xanmod kernel
 dnf copr enable rmnscnce/kernel-xanmod -y
 
