1925df6b29f4d9b2f475ac564d1fa9fa59ff5590
adner <67954372+32fca06c@users.noreply.github.com>
2022-06-07 13:12:01 +0500

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -6,7 +6,7 @@ dnf install fedora-workstation-repositories https://download1.rpmfusion.org/free
 # onlyoffice
 echo [onlyoffice]>/etc/yum.repos.d/onlyoffice.repo
 echo name=onlyoffice repo>>/etc/yum.repos.d/onlyoffice.repo
-echo baseurl=https://download.onlyoffice.com/repo/centos/main/noarch/>>/etc/yum.repos.d/onlyoffice.repo
+echo baseurl=http://download.onlyoffice.com/repo/centos/main/noarch/>>/etc/yum.repos.d/onlyoffice.repo
 echo gpgcheck=0>>/etc/yum.repos.d/onlyoffice.repo
 echo enabled=1>>/etc/yum.repos.d/onlyoffice.repo
 #
