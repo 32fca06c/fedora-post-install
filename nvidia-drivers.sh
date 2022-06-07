4dd10661a5ea591132856b2a2067f2a6afbc7cae
adner <67954372+32fca06c@users.noreply.github.com>
2022-06-07 13:07:19 +0500

    Update nvidia-drivers.sh

--- nvidia-drivers.sh
+++ nvidia-drivers.sh
@@ -2,7 +2,7 @@
 # nvidia drivers
 dnf install kmod-nvidia xorg-x11-drv-nvidia-libs.i686 xorg-x11-drv-nvidia-cuda libva-vdpau-driver -y
 sleep 2m
-modinfo -F version nvidia
+#modinfo -F version nvidia
 akmods --force
 dracut --force
 systemctl enable nvidia-{suspend,resume,hibernate}
