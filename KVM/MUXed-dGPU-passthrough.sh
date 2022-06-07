b8f17e9bc36fd756ff41ca8cee27f4c6b8835bb7
adner <67954372+32fca06c@users.noreply.github.com>
2022-06-07 13:03:36 +0500

    Update MUXed-dGPU-passthrough.sh

--- KVM/MUXed-dGPU-passthrough.sh
+++ KVM/MUXed-dGPU-passthrough.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 #nano /etc/default/grub
-intel_iommu=on iommu=pt rd.driver.pre=vfio_pci vfio-pci.ids=10de:1c20,10de:10f1
+#intel_iommu=on iommu=pt rd.driver.pre=vfio_pci vfio-pci.ids=10de:1c20,10de:10f1
 grub2-mkconfig -o /boot/grub2/grub.cfg
 reboot
-
 #lspci -nnk -s 1:
-
 #dnf install git python2 iasl nasm subversion perl-libwww-perl vim dos2unix gcc -y
 dnf install dos2unix gcc-c++ patch libuuid-devel -y
 cd /var/lib/libvirt/
